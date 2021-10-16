b8621049da5bf551bf8006063284b7bc6db6acdd
Ghassen <ghassen.boughzala@esprit.tn>
2021-10-16 15:54:32 +0100

    DiscoveryServer

---
+++ discovery-server/src/test/java/com/esprit/discovery/DiscoveryServer5twin4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esprit.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServer5twin4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
