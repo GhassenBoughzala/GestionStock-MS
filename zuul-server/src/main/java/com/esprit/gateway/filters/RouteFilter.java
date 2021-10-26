aea5b892c0a30a89aa7c048f296771255237e5ee
shady <sahdi.ettaieb@esprit.tn>
2021-10-26 12:28:36 +0100

    adding zuul gateway

---
+++ zuul-server/src/main/java/com/esprit/gateway/filters/RouteFilter.java
@@ -0,0 +1,28 @@
+package com.esprit.gateway.filters;
+
+import com.netflix.zuul.ZuulFilter;
+
+public class RouteFilter extends ZuulFilter {
+
+	@Override
+	public String filterType() {
+		return "route";
+	}
+
+	@Override
+	public int filterOrder() {
+		return 0;
+	}
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() {
+		System.out.println("Using Route Filter");
+		return null;
+	}
+
+}
